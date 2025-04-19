c630b078cb37705d97f11d3e74a5993620753acd
HiHellogobyeCode <130617826+HiHellogobyeCode@users.noreply.github.com>
2025-04-19 03:10:35 -0500

    hope

--- next.config.js
+++ next.config.js
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  images: {
-    unoptimized: true,
-  },
+  // Add any other Next.js config options below
 };
 
 module.exports = nextConfig;
