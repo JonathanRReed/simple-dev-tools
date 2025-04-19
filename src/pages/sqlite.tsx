b9277aab68c3f74db56cf84aed8e399c7844417b
HiHellogobyeCode <130617826+HiHellogobyeCode@users.noreply.github.com>
2025-04-19 03:03:34 -0500

    cleaned

--- src/pages/sqlite.tsx
+++
@@ -1,7 +0,0 @@
-import dynamic from "next/dynamic";
-
-const SQLitePlayground = dynamic(() => import("../components/SQLitePlayground"), { ssr: false });
-
-export default function SQLitePage() {
-  return <SQLitePlayground />;
-}
